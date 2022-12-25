2adac690b36c070cc65fcdbcd3019a2a52de4920
Laska <klimovskih.laska@mail.ru>
2022-12-26 01:02:22 +0700

    code

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿string[] array = array1();
+string[] result = FindLessThan(array, 3);
+
+Console.WriteLine($" Ваш массив : [{string.Join(", ", array)}]");
+Console.WriteLine ($" Сформированый по заданию массив: [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] array1() {
+   Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
